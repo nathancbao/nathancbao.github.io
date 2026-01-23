622e0cccac47dd1a766a35251fa9640694e9ee16
NBaoBao <nathanbao@gmail.com>
2026-01-23 12:30:40 -0800

    add prettier formatting

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,8 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import prettier from "eslint-config-prettier";
+import typescript from "typescript-eslint";
 
 export default defineConfig([
   globalIgnores(['dist']),
@@ -20,4 +22,5 @@ export default defineConfig([
       globals: globals.browser,
     },
   },
+  prettier
 ])
